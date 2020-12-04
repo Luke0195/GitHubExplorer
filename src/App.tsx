bdb5d2bd35ec681f9a972e0a9aaf41b4321ff130
Luke0195 <lucas201273@gmail.com>
2020-12-04 17:56:03 -0300

    chore: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1> GitHub Explorer</h1>
-  );
-}
+const App:React.FC = () => (
+  <>
+    <h1> teste</h1>
+  </>
+);
 
 export default App;
